48b6449c4027f9c5621aefbbb8e77dc3d3fe49f1
Doni <romdhani@gmail.com>
2020-02-17 13:44:01 +0700

    modify ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    /**
-     * Application configuration section
-     * http://pm2.keymetrics.io/docs/usage/application-declaration/
-     */
-    apps: [
-      // First application
-      {
-        name: 'node-url-sorthener',
-        script: 'npm',
-        args: 'run start'
-      }
-    ]
-  };
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: "node-url-shortener",
+      script: "npm",
+      args: "run prod"
+    }
+  ]
+};
